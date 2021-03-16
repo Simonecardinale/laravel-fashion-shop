29d8c39556287de1baa967260e4d400f80205ffe
Simone Cardinale <simonecardinale8@gmail.com>
2021-03-16 15:26:02 +0100

    creating link to DB

--- fashion-shop/app/Http/Controllers/HomeController.php
+++ fashion-shop/app/Http/Controllers/HomeController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Dress;
 
 class HomeController extends Controller
 {
     public function index() {
         $data = ['saluto'=>'Ciao a Tutti'];
         return view('home', $data);
+    }
+    public function dress() {
+        $data_vestiti = Dress::all();
+        $data = ['shop-vestiti' => $data_vestiti];
+        return view('vestiti', $data);
     }
 }
